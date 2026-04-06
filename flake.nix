0b0d4a2f1bb2c839e8223d429c6c50c7ad5b3529
Zeus-Deus <github.commits@widow.cc>
2026-04-07 01:01:44 +0200

    Bump version to 1.8.4

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
       in {
         packages.default = pythonPackages.buildPythonApplication rec {
           pname = "gazelle-tui";
-          version = "1.8.3";
+          version = "1.8.4";
           src = ./.;
           format = "other";
 
